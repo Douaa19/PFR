34fcd56a79a2998919d42c3681c93100dbb832f4
Douaa19 <77000956+Douaa19@users.noreply.github.com>
2021-07-19 20:18:36 +0100

    StyleForm

--- app/views/admin/accueil.php
+++ app/views/admin/accueil.php
@@ -1,5 +1,90 @@
-<?php 
+<?php include_once APPROOT . './views/inc/header-accueil-admin.php'; ?>
 
-?>
+<main>
 
-<h1><a href="<?php echo URLROOT ?>/AdminController/killSession">Kill session</a></h1>
+    <!-- Photos section -->
+    <div class="photos">
+        <a href=""><h1>photos</h1></a>
+        <div class="galery">
+            <div class="img">
+                <h3>Titre</h3>
+                <img src="../public/img/alexander-andrews-lMpoDibrEmY-unsplash.jpg" alt="">
+            </div>
+            <div class="img">
+                <h3>Titre</h3>
+                <img src="../public/img/alexander-andrews-sNPfZxrBYdQ-unsplash.jpg" alt="">
+            </div>
+            <div class="img">
+                <h3>Titre</h3>
+                <img src="../public/img/alexander-andrews-VLGWE_SumrA-unsplash.jpg" alt="">
+            </div>
+            <div class="img">
+                <h3>Titre</h3>
+                <img src="../public/img/paul-gaudriault-cwy9yVBBPxg-unsplash.jpg" alt="">
+            </div>
+            <div class="img">
+                <h3>Titre</h3>
+                <img src="../public/img/portuguese-gravity-PjlbfW1mvWE-unsplash.jpg" alt="">
+            </div>
+            <div class="img">
+                <h3>Titre</h3>
+                <img src="../public/img/alexander-andrews-sNPfZxrBYdQ-unsplash.jpg" alt="">
+            </div>
+        </div>
+    </div>
+
+    <!-- Videos section -->
+
+    <div class="videos">
+        <a href=""><h1>vidéos</h1></a>
+
+        <div id="carouselExampleControls" class="carousel slide" >
+            <div class="carousel-inner">
+              <div class="carousel-item active">
+              <img src="../public/img/kenny-leys-j27SKDa-vBg-unsplash.jpg" alt="" class="d-block w-100">
+              <!-- <video src="./public/vid/Nature Beautiful short video 720p HD.mp4" class="d-block w-100">Second video</video> -->
+              </div>
+              <div class="carousel-item">
+              <img src="../public/img/annie-spratt-ceMXSBfPoBs-unsplash.jpg" alt="" class="d-block w-100">
+              <!-- <video src="./public/vid/Nature Beautiful short video 720p HD.mp4" class="d-block w-100">First video</video> -->
+              </div>
+            </div>
+            <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="prev">
+              <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+              <span class="visually-hidden">Previous</span>
+            </button>
+            <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="next">
+              <span class="carousel-control-next-icon" aria-hidden="true"></span>
+              <span class="visually-hidden">Next</span>
+            </button>
+        </div>
+
+
+
+
+    </div>
+
+    <!-- About section -->
+
+    <div class="about">
+        <h1>a propos</h1>
+        <div class="content">
+            <img src="../public/img/brandon-erlinger-ford-jL8QFwnuOcQ-unsplash.jpg" alt="">
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ulla</p>
+        </div>
+    </div>
+    
+
+</main>
+
+
+
+
+
+
+</body>
+</html>
+
+<footer>
+<?php include_once APPROOT . '../views/inc/footer.php'; ?>
+</footer>
