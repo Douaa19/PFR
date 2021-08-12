569fca24e2a76e63ed3198c5bf36be5ca3f6f3f1
Douaa19 <77000956+Douaa19@users.noreply.github.com>
2021-08-12 01:20:11 +0100

    ResposiveDashboard

--- app/views/admin/dashboard.php
+++ app/views/admin/dashboard.php
@@ -3,24 +3,22 @@
         <main class="row m-0">
             <div class="container mt-5 col-10">
                 <h1 class="text-uppercase">Sections</h1>
-                <div class="contain">
-                    <div class="cards mt-3 mb-5">
-                        <div class="card card1 col-3">
-                            <img src="<?= URLROOT ?>/img/markus-spiske-EfhOW3cYqD8-unsplash.jpg" alt="">
-                            <div class="route strok"><h3><a href="<?php echo URLROOT ?>/PostController/dashPhoto" class="hov">PHOTOS</a></h3></div>
-                        </div>
-                        <div class="card card2 col-3">
-                            <img src="<?= URLROOT ?>/img/thomas-william-4qGbMEZb56c-unsplash.jpg" alt="">
-                            <div class="route strok"><h3><a href="<?php echo URLROOT ?>/PostController/dashVideo" class="hov">VIDEOS</a></h3></div>
-                        </div>
-                        <div class="card card3 col-3">
-                            <img src="<?= URLROOT ?>/img/viktor-talashuk-05HLFQu8bFw-unsplash.jpg" alt="">
-                            <div class="route strok"><h3><a href="<?php echo URLROOT ?>/PostController/dashFolder" class="hov">DOSSIERS</a></h3></div>
-                        </div>
-                        <div class="card card4 col-3">
-                            <img src="<?= URLROOT ?>/img/cytonn-photography-n95VMLxqM2I-unsplash.jpg" alt="">
-                            <div class="route strok"><h3><a href="<?php echo URLROOT ?>/AdminController/dashClient" class="hov">CLIENTS</a></h3></div>
-                        </div>
+                <div class="container-fluid row m-0 p-0">
+                    <div class="card col-3 p-0">
+                        <img src="<?= URLROOT ?>/img/markus-spiske-EfhOW3cYqD8-unsplash.jpg" alt="">
+                        <div class="route strok"><h3><a href="<?php echo URLROOT ?>/PostController/dashPhoto" class="hov">PHOTOS</a></h3></div>
+                    </div>
+                    <div class="card col-3 p-0">
+                        <img src="<?= URLROOT ?>/img/thomas-william-4qGbMEZb56c-unsplash.jpg" alt="">
+                        <div class="route strok"><h3><a href="<?php echo URLROOT ?>/PostController/dashVideo" class="hov">VIDEOS</a></h3></div>
+                    </div>
+                    <div class="card col-3 p-0">
+                        <img src="<?= URLROOT ?>/img/viktor-talashuk-05HLFQu8bFw-unsplash.jpg" alt="">
+                        <div class="route strok"><h3><a href="<?php echo URLROOT ?>/PostController/dashFolder" class="hov">DOSSIERS</a></h3></div>
+                    </div>
+                    <div class="card col-3 p-0">
+                        <img src="<?= URLROOT ?>/img/cytonn-photography-n95VMLxqM2I-unsplash.jpg" alt="">
+                        <div class="route strok"><h3><a href="<?php echo URLROOT ?>/AdminController/dashClient" class="hov">CLIENTS</a></h3></div>
                     </div>
                 </div>
             </div>
